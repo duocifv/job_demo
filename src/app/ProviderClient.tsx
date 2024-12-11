58a7a9e510b428c79e9e6b2c362e000d78e79e22
Duoc <lytuongdn@gmail.com>
2024-12-11 19:48:01 +0700

    update

--- src/app/ProviderClient.tsx
+++ src/app/ProviderClient.tsx
@@ -1,7 +1,9 @@
 'use client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactNode } from 'react'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import dynamic from 'next/dynamic'
+
+const ReactQueryDevtools = process.env.NODE_ENV !== 'production' ? dynamic(() => import('@tanstack/react-query-devtools').then(mod => mod.ReactQueryDevtools), { ssr: false }) : () => null;
 
 const queryClient = new QueryClient()
 
@@ -9,7 +11,9 @@ const ProviderClient = ({ children }: { children: ReactNode }) => {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools initialIsOpen={false} />
+      {process.env.NODE_ENV !== 'production' && (
+        <ReactQueryDevtools initialIsOpen={false} />
+      )}
     </QueryClientProvider>
   )
 }
